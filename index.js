dbdd1b794657f4d5e84519a7cb6b986cb38cb149
tracey <tracey.d.huang@gmail.com>
2022-05-23 02:02:44 +1000

    Convert user moves to numbers

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-//ask human for their move 
-//human enters rock paper or scissors 
-//make move case insensitive 
-play = prompt('Play your move');
-console.log(play);
-
-
 //randomly generate rock paper or scissors by computer
 //assign variables to each
 //randomly choose a variable 
@@ -28,13 +21,26 @@ function computerPlay() {
             }
 }
 
-computerPlay()
+//ask human for their move 
+//human enters rock paper or scissors 
+//make move case insensitive 
+userPlay = prompt('Play your move');
 
+let computerPlay = computerPlay();
 
+//convert user play to numbers 
+if (userPlay === 'rock') {
+    userPlay = 0;
+}
+else if (userPlay === 'paper') {
+    userPlay = 1;
+}
+else if (userPlay === 'scissors') {
+    userPlay = 0;
+} else { alert('Invalid move, try again')}
 
+//compare the plays between human and computer 
 
-
-//compare the results 
     //if same tie 
     //if rock beats scissors 
     //if rock loses paper 
@@ -43,5 +49,15 @@ computerPlay()
     //if paper lose to scissors
     //if paper wins to rock
 //display win or lose 
+
+
+
+
+
+
+
+
+
+
 
 //keep count of 5 games 
