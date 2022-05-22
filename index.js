401deeb8a5448eeb0b6f16c2880f0132164c779c
tracey <tracey.d.huang@gmail.com>
2022-05-23 01:54:42 +1000

    Create random generator for computer's move

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Hello World2");
+//Random number generator 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+//randomly generate rock paper or scissors by computer
+//assign variables to each
+//randomly choose a variable 
+function computerPlay() {
+    let randomNum = getRandomInt(3);
+    console.log(randomNum);
+
+    if (randomNum === 0)  {
+        console.log('rock');
+    }
+        else if (randomNum === 1) {
+            console.log('paper');
+        }
+            else {
+                console.log('scissors');
+            }
+}
+
+computerPlay()
+
+
+
+
+
+//compare the results 
+    //if same tie 
+    //if rock beats scissors 
+    //if rock loses paper 
+    //if scissors lose to rock 
+    //if scissors lose to rock 
+    //if paper lose to scissors
+    //if paper wins to rock
+//display win or lose 
+
+//keep count of 5 games 
