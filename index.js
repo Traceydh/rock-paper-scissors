a8e36de9f9a7bce937e1ddb64728004b3c27cce1
tracey <tracey.d.huang@gmail.com>
2022-05-23 03:34:42 +1000

    Add function for player selection and renmae variables to match requirements

--- index.js
+++ index.js
@@ -23,57 +23,50 @@ function computerPlay() {
     return pcPlay;
 }
 
-//ask human for their move 
-//human enters rock paper or scissors 
-userPlayCaseSensitive = prompt('Play your move');
-
-//make move case insensitive 
-userPlay = userPlayCaseSensitive.toLowerCase();
-
-computerPlay();
-
-//compare the plays between human and computer 
-//display win or lose 
+function playRound(playerSelection, computerSelection) {
+    //compare the plays between human and computer 
+    //display win or lose 
     //if same tie 
-if (userPlay === pcPlay) {
-    alert("Tie! No winner or loser");
-}
-    //if rock loses paper 
-else if (userPlay == 'rock' && pcPlay == "paper") {
-    alert("Computer played " + pcPlay + "\n Loser! Rock loses to paper");
-}
-    //if rock beats scissors 
-else if (userPlay == 'rock' && pcPlay == "scissors") {
-    alert("Computer played " + pcPlay + "\n Winner! Rock beats scissors");
-}
-    //if scissors lose to rock 
-else if (userPlay == 'scissors' && pcPlay == "rock") {
-    alert("Computer played " + pcPlay + "\n Loser! scissors loses to rock");
-}
-    //if scissors wins to paper
-else if (userPlay == 'scissors' && pcPlay == "paper") {
-    alert("Computer played " + pcPlay + "\n Winner! scissors beats paper");
-}   
-    //if paper lose to scissors
-else if (userPlay == 'paper' && pcPlay == "scissors") {
-    alert("Computer played " + pcPlay + "\n Loser! paper loses to scissors");
-}
-    //if paper wins to rock
-else if (userPlay == 'paper' && pcPlay == "rock") {
-    alert("Computer played " + pcPlay + "\n Winner! paper beats rock");
-} else { 
-    alert("Invalid move, try again")
-}
+    if (playerSelection === computerSelection) {
+        alert("Tie! No winner or loser");
+    }
+        //if rock loses paper 
+    else if (playerSelection == 'rock' && computerSelection == "paper") {
+        alert("Computer played " + pcPlay + "\n Loser! Rock loses to paper");
+    }
+        //if rock beats scissors 
+    else if (playerSelection == 'rock' && computerSelection == "scissors") {
+        alert("Computer played " + pcPlay + "\n Winner! Rock beats scissors");
+    }
+        //if scissors lose to rock 
+    else if (playerSelection == 'scissors' && computerSelection == "rock") {
+        alert("Computer played " + pcPlay + "\n Loser! scissors loses to rock");
+    }
+        //if scissors wins to paper
+    else if (playerSelection == 'scissors' && computerSelection == "paper") {
+        alert("Computer played " + pcPlay + "\n Winner! scissors beats paper");
+    }   
+        //if paper lose to scissors
+    else if (playerSelection == 'paper' && computerSelection == "scissors") {
+        alert("Computer played " + pcPlay + "\n Loser! paper loses to scissors");
+    }
+        //if paper wins to rock
+    else if (playerSelection == 'paper' && computerSelection == "rock") {
+        alert("Computer played " + pcPlay + "\n Winner! paper beats rock");
+    } else { 
+        alert("Invalid move, try again")
+    }
+    
 
+  }
+  
+//instead of this const playerSelection = "rock" ask human for their move 
+let playerSelectionCaseSensitive = prompt('Play your move');
+//make move case insensitive 
+let playerSelection = playerSelectionCaseSensitive.toLowerCase();
+const computerSelection = computerPlay();
 
-
-
-
-
-
-
-
-
+console.log(playRound(playerSelection, computerSelection));
 
 
 //keep count of 5 games 
