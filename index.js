49889438fac45d2968a2027d05414aa80a297c96
tracey <tracey.d.huang@gmail.com>
2022-06-12 14:46:38 +1000

    Change variable to string

--- index.js
+++ index.js
@@ -52,38 +52,38 @@ function playRound(playerSelection) {
         console.log('Tie');
     }
         //if rock loses paper 
-    else if (playerSelection == rock && computerSelection == 'paper') {
+    else if (playerSelection == 'rock' && computerSelection == 'paper') {
         alert("Computer played " + computerSelection + "\n Loser! Rock loses to paper");
         console.log('Loss');
         loss ++; 
         
     }
         //if rock beats scissors 
-    else if (playerSelection == rock && computerSelection == 'scissors') {
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
         alert("Computer played " + computerSelection + "\n Winner! Rock beats scissors");
         console.log('Win');
         win ++;
     }
         //if scissors lose to rock 
-    else if (playerSelection == scissors && computerSelection == 'rock') {
+    else if (playerSelection == 'scissors' && computerSelection == 'rock') {
         alert("Computer played " + computerSelection + "\n Loser! scissors loses to rock");
         console.log('Loss');
         loss ++;
     }
         //if scissors wins to paper
-    else if (playerSelection == scissors && computerSelection == 'paper') {
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
         alert("Computer played " + computerSelection + "\n Winner! scissors beats paper");
         console.log('Win');
         win ++;
     }   
         //if paper lose to scissors
-    else if (playerSelection == paper && computerSelection == 'scissors') {
+    else if (playerSelection == 'paper' && computerSelection == 'scissors') {
         alert("Computer played " + computerSelection + "\n Loser! paper loses to scissors");
         console.log('Loss');
         loss ++;
     }
         //if paper wins to rock
-    else if (playerSelection == paper && computerSelection == 'rock') {
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
         alert("Computer played " + computerSelection + "\n Winner! paper beats rock");
         console.log('Win');
         win ++;
